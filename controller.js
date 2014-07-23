33c228834b02fb6c38674443866d4c3732131fc4
Max <titantbx1989215@gmail.com>
2014-07-23 19:24:03 +1200

    update

---
+++ controller.js
@@ -0,0 +1,22 @@
+function controller()
+{
+this.setup = function()
+{
+$("#myHome").addClass("active");
+$("#content").load("home.html");
+}
+this.home = function()
+{
+$("#home").addClass("active");
+$("#team").removeClass("active");
+$("#content").load("team.html");
+}
+
+this.team = function()
+{
+$("#team").addClass("active");
+$("#home").removeClass("active");
+$("#content").load("home.html");
+}
+
+} 
